df9edd6cbb56cc747fdb5a12e41da2ddd59f5399
Daniel Whalen <dwhalen@integrallis.com>
2012-11-25 19:43:09 -0700

    Add validates_component method to ActiveModel::Validations

--- lib/component_validations.rb
+++ lib/component_validations.rb
@@ -1,3 +1,13 @@
 require "active_model"
 require "component_validations/version"
 require "component_validations/component_validator"
+
+module ActiveModel
+  module Validations
+    module ClassMethods
+      def validates_component(model_class, mapping)
+        validates_with ComponentValidations::ComponentValidator, :model_class => model_class, :mapping => mapping
+      end
+    end
+  end
+end
--- spec/support/models/signup.rb
+++ spec/support/models/signup.rb
@@ -2,16 +2,10 @@ class Signup
   include ActiveModel::Validations
 
   # Account validations
-  validates_with ComponentValidations::ComponentValidator, :model_class => Account, :mapping => {
-    :name => :account_name,
-    :subdomain => :account_subdomain
-  }
+  validates_component Account, :name => :account_name, :subdomain => :account_subdomain
 
   # User validations
-  validates_with ComponentValidations::ComponentValidator, :model_class => User, :mapping => {
-    :name => :user_name,
-    :email => :user_email
-  }
+  validates_component User, :name => :user_name, :email => :user_email
 
   # Account attributes
   attr_accessor :account_name, :account_subdomain
