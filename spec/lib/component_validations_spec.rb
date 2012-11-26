837d5b017fd87b50d77fcc539fc3595edff360ae
Daniel Whalen <dwhalen@integrallis.com>
2012-11-25 20:18:20 -0700

    Test that the error messages are correct

--- spec/lib/component_validations_spec.rb
+++ spec/lib/component_validations_spec.rb
@@ -18,30 +18,36 @@ describe ComponentValidations do
   it "requires an account name" do
     signup = Signup.new(@params.merge(:account_name => ""))
     expect(signup).to_not be_valid
+    expect(signup.errors[:account_name]).to eq ["can't be blank"]
   end
 
-  it "requires an account subdomains" do
+  it "requires an account subdomain" do
     signup = Signup.new(@params.merge(:account_subdomain => ""))
     expect(signup).to_not be_valid
+    expect(signup.errors[:account_subdomain]).to eq ["can't be blank", "is invalid"]
   end
 
   it "validates the format of the account subdomain" do
     signup = Signup.new(@params.merge(:account_subdomain => "foo company"))
     expect(signup).to_not be_valid
+    expect(signup.errors[:account_subdomain]).to eq ["is invalid"]
   end
 
   it "requires a user name" do
     signup = Signup.new(@params.merge(:user_name => ""))
     expect(signup).to_not be_valid
+    expect(signup.errors[:user_name]).to eq ["can't be blank"]
   end
 
   it "requires a user email" do
     signup = Signup.new(@params.merge(:user_email => ""))
     expect(signup).to_not be_valid
+    expect(signup.errors[:user_email]).to eq ["can't be blank", "is invalid"]
   end
 
   it "validates the format of the user email" do
     signup = Signup.new(@params.merge(:user_email => "jimexample.com"))
     expect(signup).to_not be_valid
+    expect(signup.errors[:user_email]).to eq ["is invalid"]
   end
 end
